e14c03cf5c02dd437526344d2e752c669cb1023e
Tamas Pallos <tamas.pallos@vincit.com>
2018-07-11 15:28:06 +0300

    filtering by date, added dev server, html render separated from data retrieval

--- hello.go
+++ hello.go
@@ -1,58 +1,61 @@
 package main
 
 import (
-    "html/template"
-    "net/http"
-    "encoding/json"
+	"html/template"
+	"net/http"
+	"encoding/json"
+	"time"
 
-    "appengine"
-    "appengine/datastore"
+	"appengine"
+	"appengine/datastore"
 )
 
 type Measurement struct {
-    Temperature float32
-    Humidity    float32
-    Date        int64
+	Temperature float32
+	Humidity    float32
+	Date        int64
 }
 
 func init() {
-    http.HandleFunc("/", showMeasurements)
-    http.HandleFunc("/add", addMeasurement)
+	http.HandleFunc("/", showMeasurements)
+	http.HandleFunc("/get", getMeasurements)
+	http.HandleFunc("/add", addMeasurement)
 }
 
 func measurementKey(c appengine.Context) *datastore.Key {
-    return datastore.NewKey(c, "Measurement", "default_measurement", 0, nil)
+	return datastore.NewKey(c, "Measurement", "default_measurement", 0, nil)
+}
+
+func getMeasurements(resp http.ResponseWriter, req *http.Request) {
+	ctx := appengine.NewContext(req)
+
+	var filter = time.Now().Add(-7 * 24 * time.Hour).UnixNano() / int64(time.Millisecond)
+	var limit = 500
+	// Ancestor queries, as shown here, are strongly consistent with the High
+	// Replication Datastore. Queries that span entity groups are eventually
+	// consistent. If we omitted the .Ancestor from this query there would be
+	// a slight chance that Greeting that had just been written would not
+	// show up in a query.
+	// Filter("Date >", filter).
+	q := datastore.NewQuery("Measurement").Ancestor(measurementKey(ctx)).Filter("Date >", filter).Order("-Date").Limit(limit)
+
+	measurements := make([]Measurement, 0, limit)
+	if _, err := q.GetAll(ctx, &measurements); err != nil {
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusCreated)
+	json.NewEncoder(resp).Encode(measurements)
 }
 
 func showMeasurements(resp http.ResponseWriter, req *http.Request) {
-    ctx := appengine.NewContext(req)
-
-    var limit = 500
-    // Ancestor queries, as shown here, are strongly consistent with the High
-    // Replication Datastore. Queries that span entity groups are eventually
-    // consistent. If we omitted the .Ancestor from this query there would be
-    // a slight chance that Greeting that had just been written would not
-    // show up in a query.
-    q := datastore.NewQuery("Measurement").Ancestor(measurementKey(ctx)).Order("-Date").Limit(limit)
-
-    measurements := make([]Measurement, 0, limit)
-    if _, err := q.GetAll(ctx, &measurements); err != nil {
-        http.Error(resp, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    measJson, err := json.Marshal(measurements)
-    if err != nil {
-        http.Error(resp, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-
-    err = chartTemplate.Execute(resp, string(measJson))
-    if err != nil {
-        http.Error(resp, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	err := chartTemplate.Execute(resp, nil)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 var chartTemplate = template.Must(template.New("book").Parse(`
@@ -71,50 +74,47 @@ var chartTemplate = template.Must(template.New("book").Parse(`
 <script src="https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.8.6/nv.d3.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
 
-window.setInterval(function() {
-  window.location.reload();
-}, 20*60*1000);
-
 <script>
-    /*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
-    nv.addGraph(function() {
-      var chart = nv.models.multiChart()
-        .margin({top: 30, right: 60, bottom: 50, left: 70})
-        .color(d3.scale.category10().range())
-        .useInteractiveGuideline(true)
-        .showLegend(true);
+	// refresh every 3 hours
+	window.setInterval(function() {
+ 		window.location.reload();
+	}, 3 * 60 * 60 * 1000); 
 
-      chart.xAxis
-        .axisLabel('Time (ms)')
-        .tickFormat(function(d) {
-          return d3.time.format('%m-%d %H:%M')(new Date(d))
-        })
-        .showMaxMin(false);
+    var chart = null;
 
-      chart.yAxis1
-        .axisLabel('C')
-        .tickFormat(d3.format('.02f'));
+    function renderChart(chartData) {
+	  nv.addGraph(function() {
+        chart = nv.models.multiChart()
+          .margin({top: 30, right: 60, bottom: 50, left: 70})
+          .color(d3.scale.category10().range())
+          .useInteractiveGuideline(true)
+          .showLegend(true);
 
-      chart.yAxis2
-        .axisLabel('%')
-        .tickFormat(d3.format('.02f'));
+        chart.xAxis
+          .axisLabel('Time (ms)')
+          .tickFormat(function(d) {
+            return d3.time.format('%m-%d %H:%M')(new Date(d))
+          })
+          .showMaxMin(false);
 
+        chart.yAxis1
+          .axisLabel('C')
+          .tickFormat(d3.format('.02f'));
 
-      var myData = getData();
+        chart.yAxis2
+          .axisLabel('%')
+          .tickFormat(d3.format('.02f'));
 
-      d3.select('#chart svg')
-        .datum(myData)
-        .call(chart);
+        d3.select('#chart svg')
+          .datum(chartData)
+          .call(chart);
+  
+        nv.utils.windowResize(function() { chart.update() });
+        return chart;
+      });
+    }
 
-      nv.utils.windowResize(function() { chart.update() });
-      return chart;
-    });
-
-    /**************************************
-     * Simple test data generator
-     */
-    function getData() {
-        var data = JSON.parse({{.}})
+    d3.json("/get", function(err, data) {
         data = _.sortBy(data, "Date");
         data = _.filter(data, function (meas) {
           return meas.Temperature > 0 &&
@@ -122,7 +122,7 @@ window.setInterval(function() {
                  moment(meas.Date).isAfter(moment().subtract(14, 'days'));
         });
 
-        return [
+        var renderData = [
           {
             values: _.map(data, function(m) {return {x: m.Date, y: m.Temperature}}),
             yAxis: 1,
@@ -138,33 +138,35 @@ window.setInterval(function() {
             color: '#2ca02c'
           }
         ];
-    }
+
+        renderChart(renderData);
+	});
 </script>
 </body>
 </html>
 `))
 
 func addMeasurement(resp http.ResponseWriter, req *http.Request) {
-    ctx := appengine.NewContext(req)
+	ctx := appengine.NewContext(req)
 
-    var meas Measurement
-    err := json.NewDecoder(req.Body).Decode(&meas)
-    if err != nil {
-        http.Error(resp, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	var meas Measurement
+	err := json.NewDecoder(req.Body).Decode(&meas)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    // We set the same parent key on every Greeting entity to ensure each Greeting
-    // is in the same entity group. Queries across the single entity group
-    // will be consistent. However, the write rate to a single entity group
-    // should be limited to ~1/second.
-    key := datastore.NewIncompleteKey(ctx, "Measurement", measurementKey(ctx))
+	// We set the same parent key on every Greeting entity to ensure each Greeting
+	// is in the same entity group. Queries across the single entity group
+	// will be consistent. However, the write rate to a single entity group
+	// should be limited to ~1/second.
+	key := datastore.NewIncompleteKey(ctx, "Measurement", measurementKey(ctx))
 
-    _, err = datastore.Put(ctx, key, &meas)
-    if err != nil {
-        http.Error(resp, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	_, err = datastore.Put(ctx, key, &meas)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    http.Redirect(resp, req, "/", http.StatusFound)
+	http.Redirect(resp, req, "/", http.StatusFound)
 }
